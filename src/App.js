b71e4a34d42510c3786dd625fe269f889720c161
purrweb <purrweb@iMac-purrweb.local>
2019-02-13 18:20:50 +0600

    1820

--- src/App.js
+++ src/App.js
@@ -7,28 +7,26 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class App extends Component {
   render() {
 
-    return (
+  return (
 
-       <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/edit">EditProfile</Link>
-        </li>
-        <li>
-          <Link to="/profile">UserProfile</Link>
-        </li>
-        <li>
-          <Link to="/">StaffMembers</Link>
-        </li>
-      </ul>
-
-
-      <Route exact path="/edit" component={EditProfile} />
-      <Route path="/profile" component={UserProfile} />
-      <Route path="/" component={UsersList} />
-    </div>
-  </Router>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/edit">EditProfile</Link>
+          </li>
+          <li>
+            <Link to="/profile">UserProfile</Link>
+          </li>
+          <li>
+            <Link to="/">StaffMembers</Link>
+          </li>
+        </ul>
+        <Route exact path="/edit" component={EditProfile} />
+        <Route path="/profile" component={UserProfile} />
+        <Route path="/" component={UsersList} />
+      </div>
+    </Router>
     );
   }
 }
